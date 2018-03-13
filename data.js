9579746e9788ded12771203d98caa588bed72b8e
jchaasis <jchaasis@gmail.com>
2018-03-13 17:57:20 -0400

    Add Gabble to projects. This should be a temporary project.

--- data.js
+++ data.js
@@ -11,15 +11,15 @@
       "description": "Vamp's sole purpose is to cure your boredom! Find and share events going around you with this easy to us map based event finder application.",
       "completionDate": '8/5/2017',
       "github": "https://github.com/jchaasis/Vamp", "live": "https://vamp.surge.sh"
-    },{"image": "http://www.placecage.com/gif/200/299",
-      "title": "Playing with Playlists",
-      "description": "For this project, I did this one thing and then after that I did this other thing. At the end, I combined them all into one large group. It was Insane!",
+    },{"image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/gabble.png",
+      "title": "Gabble",
+      "description": "Ever wonder what the offspring of Twitter and Craig's List might look like? Well here you go! While it is not much to look at, Gabble is built solely with Node.js, and utilizes Sequelize and postgreSQL to manage the Gabble database.",
       "completionDate": '9/10/2017',
-      "github": "https://github.com/jchaasis/Week-nine-project-playlist", "live": "google.com"
+      "github": "https://github.com/jchaasis/Gabble", "live": null
     }, {"image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/bentCreek.png",
       "title": "Playing with Playlists",
       "description": "The goal of this project was to take a provided mockup, and then construct the requested site with to pixel perfect. ",
       "completionDate": '9/10/2017',
-      "github": "https://github.com/jchaasis/week-one-project", "live": "google.com"
+      "github": "https://github.com/jchaasis/week-one-project", "live": null
     },
   ]
