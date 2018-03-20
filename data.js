64b85f91a9fbac5b88877092c05bfc8acc996e78
jchaasis <jchaasis@gmail.com>
2018-03-20 13:41:03 -0400

    Shorten file paths to the project path.

--- data.js
+++ data.js
@@ -1,22 +1,22 @@
 
   const projects = [
     {
-      "image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/boatYard.png",
+      "image": "Images/screenshots/boatYard.png",
       "title": "Boat Yard",
       "description": "Boat Yard is a marina management system that allows users to track when boats pull in and out of their marina.",
       "completionDate": '',
       "github": "https://github.com/jchaasis/boat-yard", "live": "https://boat-yard.surge.sh"
-    }, {"image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/vamp.png",
+    }, {"image": "Images/screenshots/vamp.png",
       "title": "Vamp",
       "description": "Vamp's sole purpose is to cure your boredom! Find and share events going around you with this easy to us map based event finder application.",
       "completionDate": '8/5/2017',
       "github": "https://github.com/jchaasis/Vamp", "live": "https://vamp.surge.sh"
-    },{"image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/gabble.png",
+    },{"image": "Images/screenshots/gabble.png",
       "title": "Gabble",
       "description": "Ever wonder what the offspring of Twitter and Craig's List might look like? Well here you go! While it is not much to look at, Gabble is built solely with Node.js, and utilizes Sequelize and postgreSQL to manage the Gabble database.",
       "completionDate": '9/10/2017',
       "github": "https://github.com/jchaasis/Gabble", "live": null
-    }, {"image": "/Users/christianhaasis/Documents/Projects/HacksisPortfolio/Images/screenshots/bentCreek.png",
+    }, {"image": "Images/screenshots/bentCreek.png",
       "title": "Playing with Playlists",
       "description": "The goal of this project was to take a provided mockup, and then construct the requested site with to pixel perfect. ",
       "completionDate": '9/10/2017',
