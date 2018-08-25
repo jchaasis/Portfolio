a97bb30fcf440c1250a5822704bcbaf797e6c882
jchaasis <jchaasis@gmail.com>
2018-08-25 18:49:40 -0400

    Add event as an parameter so that the eventListeners can register the id and display the label. This is required for firefox.

--- Javascript/Tacklebox.js
+++ Javascript/Tacklebox.js
@@ -9,35 +9,28 @@ for (let i = 0; i<lures.length; i++){
   lures[i].addEventListener('mouseleave', removeText)
 }
 
-function displayText(){
-
+function displayText(event){
   let id = event.target.id
-
   //find the current item
   let current = document.getElementById(id);
   //find its parent
-
   let parent = current.parentNode
   //create the textbox
   let textBox = document.createElement('p');
   textBox.innerHTML = id;
-
   //add style to the text
   let tStyles = textBox.style
   tStyles.position = 'absolute';
   tStyles.zindex = 20;
-
   //add id so that we can remove the label later
   textBox.setAttribute('id', `${id}Label`)
-
   //attach the new text node
   parent.insertBefore(textBox, current)
 }
 
 //remove the text box when we leave the icons
-function removeText(){
+function removeText(event){
   let id = event.target.id
-
   //find the current item
   // let current = document.getElementById(id);
   let label = document.getElementById(`${id}Label`);
